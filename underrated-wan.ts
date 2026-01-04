bca11a197ef77e3da714309fe78157a84154ceea
Marwane Kalam-Alami <marwane@kalam-alami.net>
2026-01-04 12:11:33 +0100

    Tweaking

--- underrated-wan.ts
+++ underrated-wan.ts
@@ -2,17 +2,18 @@ import { report } from "./report.ts";
 
 const EXCLUDED_GAMES = [
   // Already own
-  "Crash Bandicoot™ 4: It’s About Time" // Broken SteamSpy data?
+  "Crash Bandicoot™ 4: It’s About Time", // Broken SteamSpy data?
+  "Dungeon Keeper Gold™"
 ];
 
 (async () => {
   await report((steam, hltb) =>
     steam["Estimated owners"] < 100000 &&
     steam["Release date"] > 2020 &&
-    steam["Positive"] > 20 &&
-    steam["Positive"] < 200 &&
+    steam["Positive"] > 10 &&
+    steam["Positive"] < 100 &&
     steam["Median playtime forever"] > 0 &&
-    (steam["Positive"] / (steam["Positive"] + steam["Negative"]) >= 0.95) &&
+    (steam["Positive"] / (steam["Positive"] + steam["Negative"]) >= 0.9) &&
     !EXCLUDED_GAMES.includes(steam.Name) &&
     steam["Release date"] < 2025 && // Dataset is from April 2025
     !steam["Tags"].includes("Sexual") &&
